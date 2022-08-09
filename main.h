d18831c9e9e03a4aa95a32dee029fe335569fba9
Ayienda Brian <ayiendabrian@gmail.com>
2022-08-09 00:54:28 -0700

    struct header

--- main.h
+++ main.h
@@ -5,7 +5,24 @@
 #include<stdarg.h>
 #include<stdlib.h>
 
+/**
+ * struct print - print 
+ * @sp: to print to output
+ * @f: The print function
+ *
+ */
+
+typedef struct print
+{
+	char *sp;
+	int (*p)(va_list);
+} specifier;
+
 int _putchar(char c);
 int _printf(const char *format, ...);
+int print_char(va_list c);
+int print_int(va_list i);
+int print_string(va_list s);
+
 
 #endif
