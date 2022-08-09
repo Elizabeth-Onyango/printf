1d4c2a80b460369901dd4affa2b71b8dcd484c26
Ayienda Brian <ayiendabrian@gmail.com>
2022-08-09 12:22:50 -0700

    printf spec

--- _printf.c
+++ _printf.c
@@ -1,19 +1,83 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * _printf - printf function
- * @format: - format
+ * specifiercheck - checks if there is a valid format specifier
+ * @format: format specifier
  *
- * Return: output to stdio
+ * Return: pointer to valid function or NULL
  */
+
+int (*specifiercheck(const char *format))(va_list)
+{
+	unsigned int i;
+	specifier g[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"i", print_int},
+		{"d", print_int},
+		{NULL, NULL}
+	};
+	/* protect against null format specifier */
+	if (!format)
+		exit(99);
+	for (i = 0; g[i].sp != NULL; i++)
+	{
+		if (*(g[i].sp) == *format)
+		{
+			break;
+
+		}
+	}
+	return (g[i].p);
+}
+
+/**
+ * _printf - prints in string
+ * @format: list of argument types
+ *
+ * Return: number of characters in string
+ */
+
 int _printf(const char *format, ...)
 {
-	va_list arg;
-	int output;
+	unsigned int i = 0, length = 0;
+	va_list ap;
+	int (*p)(va_list);
 
-	va_start(arg, format);
-	output = vfprintf(stdout, format, arg);
-	va_end(arg);
-
-	return (output);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+	va_start(ap, format);
+	while (format[i])
+	{
+		for (; format[i] != '%' && format[i]; i++)
+		{
+			_putchar(format[i]), length++;
+		}
+		if (!format[i])
+			return (length);
+	if (format[i + 1] == '%')
+	{
+		_putchar('%');
+		i += 2,	length = length + 1;
+	}
+	else if (format[i + 1] == '\0')
+	{
+		i++;
+		continue;
+	}
+	else
+	{
+		p = specifiercheck(&format[i + 1]);
+	if (p == NULL)
+	{
+	_putchar(format[i]), _putchar(format[i + 1]);
+	i += 2,	length = length + 2;
+	}
+	else
+	{
+	length = length + p(ap), i += 2;
+	}}}
+va_end(ap);
+return (length);
 }
